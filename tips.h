a6e01a16976ffc3a65ea628bee33470df711708c
toku <toku.email@gmail.com>
2014-08-07 03:20:46 +0900

    refactor:blinkswitch

--- tips.h
+++ tips.h
@@ -7,3 +7,19 @@ unsigned long getdt_usec(){
 
 	return (int)max(dt,0);
 }
+
+
+//[目的]
+//.5周期でスイッチがちかちかして
+//いつ押しても反応する
+
+#define SW_PIN 12
+#define LED_PIN 13
+
+void blink_swich()
+{
+  while(!digitalRead(SW_PIN)){
+    digitalWrite(LED_PIN, (millis()%1000)<500 );
+  }
+  while(digitalRead(SW_PIN));
+}
